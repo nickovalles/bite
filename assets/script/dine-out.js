5c391ca35040aec7df862e384ac03adc95796a7d
Nilda <nick.ovalles@gmail.com>
2021-01-05 22:20:02 -0700

    <created Constants>

--- assets/script/dine-out.js
+++ assets/script/dine-out.js
@@ -1,8 +1,25 @@
 // Create Constants
+const searchButtonEl = document.getElementById("search-button");
+const cityNameEl = document.getElementById("city-name");
+const cityEntryEl = document.getElementById("city-entry");
+const reviews = document.getElementById("reviews");
+const historyEl = document.getElementById("history");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
 // API Info
+const APIKey = "4482d2f1e25804f9ddbf18853625fefe";
 
 // Form Stuff
+function getSushi(cityName) {
+  let foodURL = "https://developers.zomato.com/api/v2.1/cuisines" + cityName + "&appid=" + APIKey;
+  fetch(foodURL, {method: "GET"})
+  .then(response => response.json())
+
+    .then(function(response){
+
+    })
+}
+
 
 // Locate City Name
 
