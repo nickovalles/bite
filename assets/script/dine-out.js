0b1799c8d10b91da9ea0528c7c96977a31fc1427
Nilda <nick.ovalles@gmail.com>
2021-01-05 22:05:04 -0700

    <pseudocode for JS files>

---
+++ assets/script/dine-out.js
@@ -0,0 +1,17 @@
+// Create Constants
+
+// API Info
+
+// Form Stuff
+
+// Locate City Name
+
+// Local Storage
+
+// Event Listeners
+
+// Create Search History?
+
+// Conditions
+
+// Check Storage
